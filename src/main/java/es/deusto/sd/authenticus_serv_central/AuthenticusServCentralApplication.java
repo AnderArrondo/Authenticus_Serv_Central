5e987eb34d902746ffecff79989af4b05fe77255
Ander Arrondo <ander.arrondo.e@opendeusto.es>
2025-10-22 15:01:07 +0200

    Creación de repositorio: servidor central

---
+++ src/main/java/es/deusto/sd/authenticus_serv_central/AuthenticusServCentralApplication.java
@@ -0,0 +1,13 @@
+package es.deusto.sd.authenticus_serv_central;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticusServCentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticusServCentralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/deusto/sd/authenticus_serv_central/AuthenticusServCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package es.deusto.sd.authenticus_serv_central;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticusServCentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
