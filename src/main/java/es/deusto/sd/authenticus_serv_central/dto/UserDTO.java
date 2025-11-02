5752ce6b179f7b90d8de2fabcf90c109a17b2423
Iker Morrondo <ikermorrondo@opendeusto.es>
2025-11-02 12:03:10 +0100

    Creacion metodo sign up

--- src/main/java/es/deusto/sd/authenticus_serv_central/dto/UserDTO.java
+++ src/main/java/es/deusto/sd/authenticus_serv_central/dto/UserDTO.java
@@ -2,4 +2,57 @@ package es.deusto.sd.authenticus_serv_central.dto;
 
 public class UserDTO {
     
+    private String email;
+    private String contrasena;
+    private String nombre;
+    private int telefono;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(String email, String contrasena, String nombre, int telefono) {
+        this.email = email;
+        this.contrasena = contrasena;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO [email=" + email + ", contrasena=********, nombre=" + nombre + ", telefono=" + telefono + "]";
+    }
 }
--- src/main/java/es/deusto/sd/authenticus_serv_central/service/UserServ.java
+++ src/main/java/es/deusto/sd/authenticus_serv_central/service/UserServ.java
@@ -1,5 +1,31 @@
 package es.deusto.sd.authenticus_serv_central.service;
 
+import org.springframework.stereotype.Service;
+import es.deusto.sd.authenticus_serv_central.dto.UserDTO;
+
+@Service
 public class UserServ {
     
+    /**
+     * @param userDTO
+     * @return 
+     * @throws IllegalArgumentException 
+     */
+    public UserDTO crearUsuario(UserDTO userDTO) {
+        
+        if (userDTO.getContrasena() == null || userDTO.getContrasena().length() < 8) {
+            throw new IllegalArgumentException("La contraseña no es válida (debe tener al menos 8 caracteres).");
+        }
+        
+        
+        
+        System.out.println("SIMULACIÓN: Creando usuario...");
+        System.out.println(" - Email: " + userDTO.getEmail());
+        System.out.println(" - Nombre: " + userDTO.getNombre());
+        
+        // Por seguridad, nunca devolvemos la contraseña al cliente.
+        userDTO.setContrasena(null); 
+        
+        return userDTO;
+    }
 }
