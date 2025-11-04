9fd5838ff9290e8c639f85446b899f73b879c334
Ander Arrondo <ander.arrondo.e@opendeusto.es>
2025-11-04 16:25:06 +0100

    Datos precargados ajustados y almacenados en StateManagement

--- src/main/java/es/deusto/sd/authenticus_serv_central/service/ExpServ.java
+++ src/main/java/es/deusto/sd/authenticus_serv_central/service/ExpServ.java
@@ -26,33 +26,35 @@ public class ExpServ {
     private final Map<Long, Exped> expedientes = new HashMap<>();
 
     public ExpServ() {
-        ArchImagen img1, img2;
-        img1 = new ArchImagen("C:/imagenes/escenario_crimen.jpg");
-        img2 = new ArchImagen("C:/imagenes/prueba1.png");
+        // Imágenes de prueba
+        ArchImagen img1 = new ArchImagen("C:/imagenes/escenario_crimen.jpg");
+        ArchImagen img2 = new ArchImagen("C:/imagenes/prueba1.png");
+        ArchImagen img3 = new ArchImagen("C:/imagenes/pistas.jpg");
 
+        // Crear varios expedientes y rellenar el mapa local 'expedientes'
+        Calendar calendar = Calendar.getInstance();
         Date date;
-        Calendar calendar;
-        calendar = Calendar.getInstance();
 
         calendar.set(2024, Calendar.JANUARY, 15);
         date = calendar.getTime();
-        Exped exped = new Exped(
-            "Caso Styles", TipoExp.AMBAS, date, List.of(img1, img2));
-        // expedientes.put(exped.getId(), exped);
+        Exped exped1 = new Exped("Caso Styles", TipoExp.AMBAS, date, List.of(img1, img2));
 
         calendar.set(2020, Calendar.JUNE, 30);
         date = calendar.getTime();
-        exped = new Exped(
-            "Caso Benedicto", TipoExp.VERACIDAD, date, List.of(img1)
-        );
-        // expedientes.put(exped.getId(), exped);
+        Exped exped2 = new Exped("Caso Benedicto", TipoExp.VERACIDAD, date, List.of(img1));
 
         calendar.set(2017, Calendar.DECEMBER, 2);
         date = calendar.getTime();
-        exped = new Exped(
-            "Caso Pamela", TipoExp.INTEGRIDAD, date, List.of(img2)
-        );
-        // expedientes.put(exped.getId(), exped);
+        Exped exped3 = new Exped("Caso Pamela", TipoExp.INTEGRIDAD, date, List.of(img2, img3));
+
+        User user1 = new User("user1@gmail.com", "password123", "User One", "123456789");
+        User user2 = new User("user2@gmail.com", "password456", "User Two", "987654321");
+
+        StateManagement.usuarioExpediente.put(user1, new ArrayList<>(List.of(exped1, exped2)));
+        StateManagement.usuarioExpediente.put(user2, new ArrayList<>(List.of(exped3)));
+
+        StateManagement.tokenUsuario.put("token_user1", user1);
+        StateManagement.tokenUsuario.put("token_user2", user2);
     }
 
     public ExpedDTO crearExpediente(ExpedDTO expedDTO, String token) throws IllegalArgumentException, ParseException {
