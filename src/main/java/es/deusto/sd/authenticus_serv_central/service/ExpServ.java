c45e31001b4536beeb2bdf2656abbb94e334b295
Ander Arrondo <ander.arrondo.e@opendeusto.es>
2025-11-10 18:42:32 +0100

    fix crearExpediente()

--- src/main/java/es/deusto/sd/authenticus_serv_central/service/ExpServ.java
+++ src/main/java/es/deusto/sd/authenticus_serv_central/service/ExpServ.java
@@ -45,6 +45,9 @@ public class ExpServ {
         if(existeExpediente(exped, usuario)) {
             throw new IllegalArgumentException("El expediente ya existe.");
         }
+        if(StateManagement.usuarioExpediente.get(usuario) == null) {
+            StateManagement.usuarioExpediente.put(usuario, new ArrayList<>());
+        }
         StateManagement.usuarioExpediente.get(usuario).add(exped);
 
         return toDTO(exped);
